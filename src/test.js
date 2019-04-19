b7e811f589396b4f53cb3fb03d507dd8caebe67e
Lachlan Blake <lachlan.blake10@gmail.com>
2019-04-20 00:32:34 +1000

    Delete test.js

--- src/test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-
-const helloWorld = require('./index.js');
-
-helloWorld.handler(null, null, (error, data) => {
-	assert.deepStrictEqual(data.slice(-12), ' From GitHub');
-	console.log('PASS');
-});
